4d9d6a0e320dc31e7ae2c74b9daa01382652628e
sjh2428 <pok_gare@naver.com>
2019-10-15 14:57:49 +0900

    feat: add room model
    방 데이터를 저장할 tbl_room  모델 추가

---
+++ src/database/models/room.js
@@ -0,0 +1,110 @@
+const model = (sequelize, DataTypes) => {
+  const Room = sequelize.define(
+    'tbl_rooms',
+    {
+      room_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        allowNull: false,
+      },
+      room_url: {
+        type: DataTypes.STRING(65535),
+        allowNull: false,
+      },
+      room_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      room_summary: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      room_picture_url: {
+        type: DataTypes.STRING(65535),
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      country: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      property_type: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      room_type: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      accommodates: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+      },
+      bathrooms: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      bedrooms: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      beds: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      bed_type: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      amenities: {
+        type: DataTypes.STRING(1000),
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      weekly_price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      monthly_price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      price_per_extra_people: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      reviewers: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      reviews_score: {
+        type: DataTypes.FLOAT.UNSIGNED,
+        allowNull: false,
+      },
+      host_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+      tableName: 'tbl_rooms',
+      timestamps: true,
+      paranoid: true,
+    },
+  );
+
+  return Room;
+};
+
+export default model;
