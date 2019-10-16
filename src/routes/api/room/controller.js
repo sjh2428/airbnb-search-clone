45da9e75a022da9a26bbe4172ce44333cd7b16d8
sjh2428 <pok_gare@naver.com>
2019-10-16 13:34:36 +0900

    fix: wrapping try-catch to controllers in /api/room

--- src/routes/api/room/controller.js
+++ src/routes/api/room/controller.js
@@ -29,46 +29,53 @@ const controller = {
       host_id,
     } = req.body;
 
-    await models[tblName.room].bulkCreate(
-      [
-        {
-          room_id,
-          room_url,
-          room_name,
-          room_summary,
-          room_picture_url,
-          city,
-          state,
-          country,
-          property_type,
-          room_type,
-          accommodates,
-          bathrooms,
-          bedrooms,
-          beds,
-          bed_type,
-          amenities,
-          price,
-          weekly_price,
-          monthly_price,
-          price_per_extra_people,
-          reviewers,
-          reviews_score,
-          host_id,
-        },
-      ],
-      { returning: true },
-    );
-
-    res.status(201).end();
+    try {
+      const result = await models[tblName.room].bulkCreate(
+        [
+          {
+            room_id,
+            room_url,
+            room_name,
+            room_summary,
+            room_picture_url,
+            city,
+            state,
+            country,
+            property_type,
+            room_type,
+            accommodates,
+            bathrooms,
+            bedrooms,
+            beds,
+            bed_type,
+            amenities,
+            price,
+            weekly_price,
+            monthly_price,
+            price_per_extra_people,
+            reviewers,
+            reviews_score,
+            host_id,
+          },
+        ],
+        { returning: true },
+      );
+      res.status(201).json(result);
+    } catch (e) {
+      res.status(500).end();
+    }
   },
   async get(req, res) {
-    let { where, limit, offset } = req.query;
-    where = where ? JSON.parse(where) : {};
-    if (!limit) limit = 20;
-    if (!offset) offset = 0;
-    const result = await models[tblName.room].findAndCountAll({ where, limit, offset });
-    res.json(result);
+    try {
+      let { where, limit, offset } = req.query;
+      where = where ? JSON.parse(where) : {};
+      if (!limit) limit = 20;
+      if (!offset) offset = 0;
+      const result = await models[tblName.room].findAndCountAll({ where, limit, offset });
+      res.json(result);
+    } catch (e) {
+      res.status(500).end();
+    }
   },
 };
 
