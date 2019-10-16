40cdf02f93a2431270c6843b9938ad33e1b9968d
sjh2428 <pok_gare@naver.com>
2019-10-16 15:00:30 +0900

    feat: add /api/user GET controller
    user 객체를 넘겨주는 controller 구현

--- src/routes/api/user/controller.js
+++ src/routes/api/user/controller.js
@@ -1,7 +1,22 @@
+import models from '../../../database';
+import tblName from '../../../database/name';
+
 // url: /api/user
 const controller = {
   post(req, res, next) {},
-  get(req, res, next) {},
+  async get(req, res) {
+    const { id } = req.query;
+    try {
+      const user = await models[tblName.user].findOne({
+        where: {
+          user_id: id,
+        },
+      });
+      res.json(user);
+    } catch (e) {
+      res.status(500).end();
+    }
+  },
 };
 
 export default controller;
