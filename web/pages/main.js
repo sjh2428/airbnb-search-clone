55cd971c49e8136a71caf8af21bfe3f54dbfc495
sjh2428 <pok_gare@naver.com>
2019-10-22 17:05:04 +0900

    feat: filter를 모아둘 filter wrapper css 추가

--- web/pages/main.js
+++ web/pages/main.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Filters from '../components/filters';
+import styled from 'styled-components';
+
+const FilterWrapper = styled.div`
+  width: 100%;
+  border: 1px solid #e4e4e4;
+  padding: 5px;
+`;
 
 const Guests = () => {
   return (
-    <>
+    <FilterWrapper>
       <Filters />
-    </>
+    </FilterWrapper>
   );
 };
 
