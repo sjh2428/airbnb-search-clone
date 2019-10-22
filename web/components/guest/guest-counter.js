2b65328fc7fdd7eb28474e56cacd44d23407f210
sjh2428 <pok_gare@naver.com>
2019-10-22 09:56:31 +0900

    fix: action type 상수화 in components/guest/guest-counter.js

--- web/components/guest/guest-counter.js
+++ web/components/guest/guest-counter.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import variables from './guest-variables';
+
+const { ACTION_INCREMENT, ACTION_DECREMENT } = variables;
 
 const GuestCounter = props => {
   const { type, cnt, who, handler } = props;
@@ -6,9 +9,9 @@ const GuestCounter = props => {
   return (
     <div>
       {type}
-      <button onClick={() => handler({ type: 'decrement', who })}>-</button>
+      <button onClick={() => handler({ type: ACTION_DECREMENT, who })}>-</button>
       {cnt}
-      <button onClick={() => handler({ type: 'increment', who })}>+</button>
+      <button onClick={() => handler({ type: ACTION_INCREMENT, who })}>+</button>
     </div>
   );
 };
