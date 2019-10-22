97e8ba8e2d2f3beeb0efe6da42860ad692241d2a
sjh2428 <pok_gare@naver.com>
2019-10-22 15:52:27 +0900

    chore: components/guest/index.js 기능 수정 없는 코드 재배치

--- web/components/guest/index.js
+++ web/components/guest/index.js
@@ -7,28 +7,26 @@ import { Popover, Button } from 'antd';
 
 const { TOTAL_STATE_KEY, ADULT_STATE_KEY, CHILD_STATE_KEY, INFANT_STATE_KEY, ACTION_INIT } = variables;
 
-const SetPeople = () => {
-  const initState = {
-    [TOTAL_STATE_KEY]: 0,
-    [ADULT_STATE_KEY]: 0,
-    [CHILD_STATE_KEY]: 0,
-    [INFANT_STATE_KEY]: 0,
-  };
+const initState = {
+  [TOTAL_STATE_KEY]: 0,
+  [ADULT_STATE_KEY]: 0,
+  [CHILD_STATE_KEY]: 0,
+  [INFANT_STATE_KEY]: 0,
+};
 
+const SetPeople = () => {
   const [state, dispatch] = useReducer(setGuestsReducer, initState);
   const [visible, setVisible] = useState(false);
   const handleVisible = v => setVisible(v);
 
-  const guestCounterJSX = guestCounters(state).map((counter, idx) => {
-    const { who, type, cnt } = counter;
-    const props = { key: idx, who, type, cnt, handler: dispatch };
-    return <GuestCounter {...props} />;
-  });
-
   return (
     <Popover
       placement="bottomLeft"
-      title={guestCounterJSX}
+      title={guestCounters(state).map((counter, idx) => {
+        const { who, type, cnt } = counter;
+        const props = { key: idx, who, type, cnt, handler: dispatch };
+        return <GuestCounter {...props} />;
+      })}
       content={
         <div>
           <Button onClick={() => dispatch({ type: ACTION_INIT })} disabled={state[TOTAL_STATE_KEY] ? false : true}>
