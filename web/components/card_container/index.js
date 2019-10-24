35ee85905cc259684b8f38b1463aa0b1a13317b9
sjh2428 <pok_gare@naver.com>
2019-10-24 20:31:55 +0900

    fix: fetch 분리 components/card_container/index.js
    fetch 분리 (useFetch.js)
    무한 스크롤 구현 완료

--- web/components/card_container/index.js
+++ web/components/card_container/index.js
@@ -6,6 +6,7 @@ import L from '../../fx/L';
 import _ from '../../fx/_';
 import { EntireContext } from '../../contexts/entire';
 import { PRICE_STEP, EXCHANGE_RATE } from '../price/price-variables';
+import useFetch from './useFetch';
 
 const Container = styled.div`
   width: 100%;
@@ -15,70 +16,51 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const initData = {
-  card: [],
-  totalCount: null,
-  fetchData: [],
-  loading: true,
-};
-
 const ROW_PRICE = price => price * EXCHANGE_RATE * PRICE_STEP;
 const TAKE_COUNT = 20;
 
 const CardContainer = () => {
-  const [containerState, setContainerdState] = useState(initData);
+  const [containerState, setContainerState] = useState(TAKE_COUNT);
   const {
     guest: { state: guestState },
     date: { startDate, endDate },
     price: { minPrice, maxPrice },
   } = useContext(EntireContext).filter;
 
-  useEffect(() => {
-    fetch(`http://${REMOTE}:${REMOTE_PORT}/api/room`)
-      .then(res => res.json())
-      .then(json => {
-        const { rows, count } = json;
-        setContainerdState({
-          loading: false,
-          totalCount: `${count}개의 숙소중...`,
-          card: _.go(
-            rows,
+  const [data, loading] = useFetch(`http://${REMOTE}:${REMOTE_PORT}/api/room`);
+
+  if (!loading) {
+    const scrollHandler = e => {
+      const { scrollHeight, clientHeight } = document.body;
+      if (scrollHeight - clientHeight === window.scrollY) {
+        setContainerState(containerState + TAKE_COUNT);
+        window.removeEventListener('scroll', scrollHandler);
+      }
+    };
+
+    window.addEventListener('scroll', scrollHandler);
+
+    return (
+      <>
+        <h2>{data.rows.length + '개의 숙소'}</h2>
+        <hr style={{ border: '1px solid #EBEBEB' }} />
+        <Container className={'card-container'}>
+          {_.go(
+            data.rows,
             L.filter(row => row.accommodates >= guestState.totalGuests),
             L.filter(row => ROW_PRICE(row.price) >= minPrice),
             L.filter(row => ROW_PRICE(row.price) <= maxPrice),
             L.map(row => <CardGrid key={row.room_id} {...row} />),
-            _.take(TAKE_COUNT),
-          ),
-          fetchData: rows,
-        });
-        window.addEventListener('scroll', scrollHandler(rows));
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    return () => window.removeEventListener('scroll', scrollHandler());
-  }, [guestState, startDate, endDate, minPrice, maxPrice]);
-
-  const scrollHandler = rows => () => {
-    const { scrollHeight, clientHeight } = document.body;
-    if (scrollHeight - clientHeight === window.scrollY) {
-      const next = _.go(
-        rows,
-        L.map(row => <CardGrid key={row.room_id} {...row} />),
-        _.take(document.querySelector('.card-container').children.length + TAKE_COUNT),
-      );
-      setContainerdState({
-        ...containerState,
-        card: next,
-      });
-    }
-  };
+            _.take(containerState),
+          )}
+        </Container>
+      </>
+    );
+  }
 
   return (
     <>
-      <h2>{containerState.totalCount ? containerState.totalCount : '불러오는 중...'}</h2>
-      <hr style={{ border: '1px solid #EBEBEB' }} />
-      <Container className={'card-container'}>{containerState.card}</Container>
+      <h2>{'불러오는 중...'}</h2>
     </>
   );
 };
---
+++ web/components/card_container/useFetch.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+const useFetch = url => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  async function fetchUrl() {
+    const response = await fetch(url);
+    const json = await response.json();
+    setData(json);
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+  return [data, loading];
+};
+
+export default useFetch;
