c1b2574bbd33d86d97bfb1518938fb29626f8524
sjh2428 <pok_gare@naver.com>
2019-10-24 17:16:24 +0900

    feat: add infinite scroll rendering

--- web/components/card_container/index.js
+++ web/components/card_container/index.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import styled from 'styled-components';
 import CardGrid from '../card';
 import { REMOTE, REMOTE_PORT } from '../../env';
 import L from '../../fx/L';
 import _ from '../../fx/_';
+import { EntireContext } from '../../contexts/entire';
 
 const Container = styled.div`
   width: 100%;
@@ -13,29 +14,62 @@ const Container = styled.div`
   justify-content: center;
 `;
 
+const initData = {
+  card: [],
+  totalCount: null,
+  fetchData: [],
+  loading: true,
+};
+
 const CardContainer = () => {
-  const [cardData, setCardData] = useState(null);
-  const [totalCardCount, setTotalCardCount] = useState(null);
+  const [containerState, setContainerdState] = useState(initData);
+  const {
+    guest: { state },
+    date: { startDate, endDate },
+    price: { minPrice, maxPrice },
+  } = useContext(EntireContext).filter;
+  const filters = [state, startDate, endDate, minPrice, maxPrice];
 
   useEffect(() => {
+    if (!containerState.loading) return;
     fetch(`http://${REMOTE}:${REMOTE_PORT}/api/room`)
       .then(res => res.json())
       .then(json => {
-        setTotalCardCount(`${json.count}개의 숙소`);
-        setCardData(_.go(json.rows, L.map(row => <CardGrid key={row.room_id} {...row} />), _.take(20)));
+        const { rows, count } = json;
+        setContainerdState({
+          loading: false,
+          totalCount: `${count}개의 숙소`,
+          card: _.go(rows, L.map(row => <CardGrid key={row.room_id} {...row} />), _.take(20)),
+          fetchData: rows,
+        });
+        window.addEventListener('scroll', scrollHandler(rows));
       })
       .catch(err => {
         console.log(err);
-        setCardData('Loading Error');
-        setTotalCardCount('Loading Error');
       });
-  }, []);
+    return () => window.removeEventListener('scroll', scrollHandler());
+  }, [...filters]);
+
+  const scrollHandler = rows => () => {
+    const { scrollHeight, clientHeight } = document.body;
+    if (scrollHeight - clientHeight === window.scrollY) {
+      const next = _.go(
+        rows,
+        L.map(row => <CardGrid key={row.room_id} {...row} />),
+        _.take(document.querySelector('.card-container').children.length + 20),
+      );
+      setContainerdState({
+        ...containerState,
+        card: next,
+      });
+    }
+  };
 
   return (
     <>
-      <h2>{totalCardCount ? totalCardCount : '불러오는 중...'}</h2>
+      <h2>{containerState.totalCount}</h2>
       <hr style={{ border: '1px solid #EBEBEB' }} />
-      <Container>{cardData}</Container>
+      <Container className={'card-container'}>{containerState.card}</Container>
     </>
   );
 };
